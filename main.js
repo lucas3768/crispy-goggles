b064483fdb3886ed2914b1ce40d4f1966cc7bf8d
lucas3768 <119453950+lucas3768@users.noreply.github.com>
2023-08-08 18:49:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ var model_status = ""
 function modelloaded() {
     console.log("modelo carregado")
     model_status = true
-    Detectordeobjetos.detect(i,gotresults)
+
 }
 function gotresults(error,results) {
-    if(error)
-    console.log(error)
-    else
+    if(error){
+    console.log(error)}
+    else{
     console.log(results)
     objetos = results
-}
+}}
 function preload() {
     video = createVideo("video.mp4")
 }
@@ -22,6 +22,19 @@ video.hide()
 }
 function draw() {
     image(video,0,0,480,380)
+    if (model_status != "") {
+        detector_objetos.detect(video,gotresults)
+        document.getElementById("status").innerHTML= "objetos detectados"
+        document.getElementById("numeroobjetos").innerHTML = "numero de objtos detectados" + objetos.length
+        for (i = 0; i < objetos.length; i++) {
+            text(objetos[i].label,objetos[i].x,objetos[i].y)
+            fill("#008000")
+            noFill()
+            stroke("#008000")
+            rect(objetos[i].x,objetos[i].y,objetos[i].width,objetos[i].height)
+            
+        }
+    }
 }
 function start() {
     detector_objetos = ml5.objectDetector('cocossd',modelloaded)
